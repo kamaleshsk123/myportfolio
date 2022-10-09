6596ac03ac2556b9f78495ec0887a566e67ef243
kamaleshsk123 <shanmugakamalesh123@gmail.com>
2022-10-09 09:38:41 +0530

    loading complete

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -45,3 +45,14 @@ window.addEventListener("load", () => {
         dayNight.querySelector("i").classList.add("fa-moon");
     }
 })
+
+const loader = document.querySelector('.loader');
+window.addEventListener('load', () => {
+    setTimeout(() =>{
+        loader.classList.add('.fadeOut');
+        setTimeout(() =>{
+            loader.style.display = 'none';
+        },1000);
+    },5000);
+    
+});
